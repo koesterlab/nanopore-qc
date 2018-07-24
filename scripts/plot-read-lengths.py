ecbc52a2c89f249fce80218f229626d5c8769a64
Johannes Köster <johannes.koester@tu-dortmund.de>
2018-07-24 17:35:12 +0200

    fixes and plots

--- scripts/plot-read-lengths.py
+++ scripts/plot-read-lengths.py
@@ -1,11 +1,24 @@
 import json
 
+import pandas as pd
 import matplotlib
 matplotlib.use("Agg")
-
+import seaborn as sns
 import matplotlib.pyplot as plt
 
 
 fastqc = json.load(open(snakemake.input[0]))
 
-import pdb; pdb.set_trace()
+lendist = pd.DataFrame.from_records(fastqc["Sequence Length Distribution"]["contents"])
+lendist["Length"] = lendist["Length"].apply(lambda l: int(l.split("-")[0]) if isinstance(l, str) else l)
+lendist = lendist[lendist["Count"] > 1]
+if not lendist.empty:
+    sns.barplot(x="Length", y="Count", data=lendist, color="#649600")
+
+sns.despine()
+plt.xlabel("min read length")
+plt.ylabel("count")
+plt.xticks(rotation="vertical")
+
+plt.savefig(snakemake.output[0], bbox_inches="tight")
+
